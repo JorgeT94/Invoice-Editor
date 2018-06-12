85bd20f7e9f2ead4c24eb55ccdd1c3494d2730c7
Jorge Treviño <jlts.01.94@gmail.com>
2018-06-11 22:15:11 -0600

    Invoice stored items design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { Item } from './Models/Item';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+	items: Item[] = [];
+
+	constructor() {
+		this.items.push(
+			{
+				name: 'Widget',
+				quantity: 2,
+				price: 5,
+				modifyPrice: false
+			},
+			{
+				name: 'Cog',
+				quantity: 2,
+				price: 15.994,
+				modifyPrice: false
+			}
+		);
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+// import { registerLocaleData } from "@angular/common";
+// import localeEs from '@angular/common/locales/es';
+// import localeEsExtra from '@angular/common/locales/extra/es';
+// registerLocaleData(localeEs);
 
 import { AppComponent } from './app.component';
 
-
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		FormsModule
+	],
+	providers: [
+		// {provide: LOCALE_ID, useValue: 'es-MX'},
+	],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
